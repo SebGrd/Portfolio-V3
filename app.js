d75589a797bac95c80392a38671692757dfe5a03
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2020-01-30 11:44:35 +0100

    Gestion port

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,25 +38,31 @@ app.use('/mes-competences', competencesRouter);
 app.use('/veille', veilleRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
 
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log(`App listening at http://${host}:${port}`);
+let port;
+if (process.env.PORT) {
+    port = process.env.PORT;
+} else {
+    port = 8080;
+}
+const server = app.listen(port, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`App listening at http://${host}:${port}`);
 });
