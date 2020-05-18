295d31c6f9a6dd3b96d94e831db536410819f1a3
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2020-05-18 20:03:33 +0200

    Veille

--- routes/veille.js
+++ routes/veille.js
@@ -1,19 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Parser = require('rss-parser')
+const parser = new Parser();
 
 /* GET Veille page */
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res) => {
 
-    // let error;
-    //
-    // fetch('https://www.silicon.fr/feed').then( (res) =>{
-    //     console.log(res);
-    // }).catch( () => {
-    //     console.log('Fetch Error');
-    //     error += 'Fetch Error';
-    // });
+    let techFeed = await parser.parseURL('https://www.silicon.fr/feed');
+    let juriFeed = await parser.parseURL('https://www.legalis.net/feed/');
 
-    res.render('veille', {title: 'Veille'})
+
+    res.render('veille', {title: 'Veille', techFeed, juriFeed})
 });
 
 module.exports = router;
